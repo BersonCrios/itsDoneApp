2858d580572b4b0e8d73bcbb7814831e150384fe
Guilherme <bersondev@gmail.com>
2019-07-08 17:25:04 -0300

    iniciando perfil

---
+++ app/src/main/java/io/github/berson/itsdone/Activities/PerfilActivity.java
@@ -0,0 +1,16 @@
+package io.github.berson.itsdone.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.berson.itsdone.R;
+
+public class PerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+}
